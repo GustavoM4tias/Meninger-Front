e87b8207b159429779de7ed875756ddb5e88cb17
GustavoM4tias <faculdogu@gmail.com>
2024-11-12 14:22:32 -0300

    Api Url

--- src/config/apiAuthUrl.js
+++ src/config/apiAuthUrl.js
@@ -1,4 +1,5 @@
 // src/config/apiAuthUrl.js
-const AUTH_URL = 'http://localhost:5000/api/auth'; 
+// const AUTH_URL = 'http://localhost:5000/api/auth'; 
+const AUTH_URL = 'https://node-back-eight.vercel.app/api/auth'; 
 
 export default AUTH_URL;
