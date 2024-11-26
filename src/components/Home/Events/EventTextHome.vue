d8825669effe38ec6221d7d095e93617cea483a2
GustavoM4tias <faculdogu@gmail.com>
2024-11-26 17:59:24 -0300

    Events ajust

--- src/components/Home/Events/EventTextHome.vue
+++ src/components/Home/Events/EventTextHome.vue
@@ -22,14 +22,18 @@ const formatDate = (dateString) => {
 </script>
 
 <template>
-    <div class="border-b-2 border-gray-600 hover:bg-gray-800 cursor-pointer px-5 py-4 text-xl flex">
+    <RouterLink :to="{
+        path: '/events',
+        query: { busca: event.title, section: 'geral' }
+    }" class="border-b-2 border-gray-600 hover:bg-gray-800 cursor-pointer px-5 py-4 text-xl flex">
         <div class="date text-center text-md rounded-br-xl rounded-tl-lg bg-gray-500 py-1 px-2 mr-4">
             <p class="day font-semibold">{{ formatDate(event.event_date).day }}</p>
             <p class="month -mt-2">{{ formatDate(event.event_date).month }}</p>
         </div>
         <div class="text w-full truncate flex flex-col justify-center">
             <div class="title">{{ event.title }}</div>
-            <p class="hour text-sm text-gray-300">Hora: {{ formatDate(event.event_date).hours }}:{{ formatDate(event.event_date).minutes }}</p>
+            <p class="hour text-sm text-gray-300">Hora: {{ formatDate(event.event_date).hours }}:{{
+                formatDate(event.event_date).minutes }}</p>
         </div>
-    </div>
+    </RouterLink>
 </template>
