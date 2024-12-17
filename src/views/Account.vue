aad889a6de51f58af4e6c1b7681354de0fed406a
GustavoM4tias <faculdogu@gmail.com>
2024-12-17 17:26:43 -0300

    Router adjust Account Form 2

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -2,13 +2,13 @@
     <div class="bg-gray-300 dark:bg-gray-800 flex flex-col px-4 md:px-8 h-[calc(100%-4rem)] w-full">
 
         <div class="bg-gray-200 dark:bg-gray-600 m-auto w-5/6 lg:w-2/6 rounded-xl shadow-xl p-6">
-            <AccountForm />
+            <Form />
         </div>
 
     </div>
 </template>
 
 <script setup>
-import AccountForm from '../components/auth/AccountForm.vue'
+import Form from '../components/auth/Form.vue'
 
 </script>
