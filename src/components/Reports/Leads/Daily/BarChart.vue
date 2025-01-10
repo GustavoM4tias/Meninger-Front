395c04c38c23256ce21b164199e339b63e06e2ed
GustavoM4tias <faculdogu@gmail.com>
2025-01-10 17:01:24 -0300

    Leads Modal Details

--- src/components/Reports/Leads/Daily/BarChart.vue
+++ src/components/Reports/Leads/Daily/BarChart.vue
@@ -7,24 +7,23 @@
         :options="empreendimentosUnicos" placeholder="Selecionar Empreendimento" required />
     </div>
     <div class="my-2 w-60 mx-auto flex flex-col">
-      <!-- <label class="block text-sm font-medium text-center mb-1">Selecionar Data</label> -->
-      <input
-        type="date"
-        v-model="dataSelecionada"
-        :max="dataHoje"
-        class="px-2 py-1 text-sm text-center border mx-auto text-gray-700 dark:text-gray-300 placeholder:text-gray-200 dark:bg-gray-500 border-gray-100 dark:border-gray-600 rounded-lg shadow-sm w-3/6"
-      />
+      <input type="date" v-model="dataSelecionada" :max="dataHoje"
+        class="px-2 py-1 text-sm text-center border mx-auto text-gray-700 dark:text-gray-300 placeholder:text-gray-200 dark:bg-gray-500 border-gray-100 dark:border-gray-600 rounded-lg shadow-sm w-3/6" />
     </div>
 
     <div class="relative">
-      <div class="text-3xl absolute right-0 -top-5 z-10">
+      <div class="text-3xl absolute right-0 -top-5">
         <i class="far fa-file-image cursor-pointer hover:scale-[103%] text-gray-700 hover:text-gray-800 dark:text-gray-50 dark:hover:text-gray-100"
           @click="salvarComoImagem"></i>
       </div>
-      <BarChart ref="chartDiario" class="h-[36vh]" :chart-data="chartData" :options="chartOptions" />
+      <BarChart ref="chartDiario" class="h-[36vh]" :chart-data="chartData" :options="chartOptions"
+        @click="abrirModalLeads" />
     </div>
-    <LoadingComponents v-if="carregando" />
+
+    <LoadingComponents v-if="carregando" /> 
+    <!-- Modal de Leads -->
   </div>
+  <ModalLeads :leads="leadsDoDia" :modalVisivel="modalVisivel" @update:modalVisivel="modalVisivel = $event" /> 
 </template>
 
 <script setup>
@@ -33,6 +32,7 @@ import { BarChart } from 'vue-chart-3';
 import { Chart as ChartJS, registerables } from 'chart.js';
 import LoadingComponents from '../../../Loading/LoadingComponents.vue';
 import Select from '../../../../components/UI/Select.vue';
+import ModalLeads from '../components/ModalLeads.vue';
 import { useLeadsStore } from '../../../../stores/leadStore';
 
 const leadsStore = useLeadsStore();
@@ -40,6 +40,8 @@ const carregando = ref(true);
 const chartDiario = ref(null);
 const dataHoje = new Date().toISOString().split('T')[0]; // Data atual formatada
 const dataSelecionada = ref(dataHoje); // Inicializa com a data de hoje 
+const modalVisivel = ref(false); // Controle de visibilidade do modal
+const leadsDoDia = ref([]); // Lista de leads que será exibida no modal
 
 watchEffect(() => {
   if (leadsStore.leads.length > 0) {
@@ -94,6 +96,8 @@ const obterResumoLeads = (leads, data) => {
   return {
     diaSelecionado: leadsDiaSelecionado.length,
     diaAnterior: leadsDiaAnterior.length,
+    leadsDiaSelecionado,
+    leadsDiaAnterior,
   };
 };
 
@@ -160,5 +164,21 @@ const salvarComoImagem = () => {
   link.download = 'relatorio_diario.png';
   link.href = canvas.toDataURL();
   link.click();
+};
+
+// Abre o modal com os leads do dia selecionado
+const abrirModalLeads = (event) => {
+  const chart = chartDiario.value.chartInstance;
+  const pontosClicados = chart.getElementsAtEventForMode(event, 'nearest', { intersect: true }, false);
+
+  if (pontosClicados.length) {
+    const index = pontosClicados[0].index;
+    if (index === 1) {
+      leadsDoDia.value = resumoLeads.value.leadsDiaSelecionado;
+    } else if (index === 0) {
+      leadsDoDia.value = resumoLeads.value.leadsDiaAnterior;
+    }
+    modalVisivel.value = true;
+  }
 };
 </script>
