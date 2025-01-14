82d128b2598d0f0a16afcbd598e3dabd2c4d078f
GustavoM4tias <faculdogu@gmail.com>
2025-01-14 17:55:45 -0300

    nav adjust

---
+++ src/components/UI/InProgress.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex h-[calc(100vh-4rem)] text-4xl">
+        <p class="m-auto relative text-center">Página em desenvolvimento! <br> <span class="text-lg text-gray-400">Verifique os prazos com seu Gestor.</span>
+        </p>
+        <i class="fa-solid fa-spinner fa-spin-pulse text-9xl absolute left-[40%] md:left-[48.5%] top-72"></i>
+    </div>
+</template>
---
+++ src/views/Reports/Vendas.vue
@@ -0,0 +1,9 @@
+<script setup>
+import InProgress from '../../components/UI/InProgress.vue';
+</script>
+
+<template>
+
+    <InProgress />
+
+</template>
