14c8020b4450e129d60d620f1567944253cc0a64
GustavoM4tias <faculdogu@gmail.com>
2025-03-28 17:22:44 -0300

    Leads card Update

--- src/views/Reports/Leads.vue
+++ src/views/Reports/Leads.vue
@@ -1,23 +1,41 @@
 <template>
   <div class="w-full h-[calc(100%-4rem)] relative overflow-hidden flex">
     <!-- Área principal -->
-    <div class="w-9/12 p-4">
+    <div class="w-9/12 ps-4 py-4">
       <div class="flex items-center pb-2">
         <h1 class="text-xl md:text-2xl font-bold">Leads</h1>
         <Favorite :router="'/reports/leads'" :section="'Leads'" />
-      </div>
+      </div> 
 
       <!-- https://preview--demo-data-analytics.lovable.app/
       https://lovable.dev/projects/01805fd4-daec-4cf7-a1e3-966d649fa0e9 -->
 
       <div class="cards flex w-full gap-4 mb-3">
-        <Card :title="'Todos os leads'" :icon="'fas fa-user'" :value="leads.length" :label="'Dentro do periodo'" />
-        <Card :title="'Aguardando Atendimento'" :icon="'fas fa-user'" :value="aguardando.length"
-          :label="'Dentro do periodo'" />
-        <Card :title="'Em Atendimento'" :icon="'fas fa-user'" :value="atendimentos.length"
-          :label="'Dentro do periodo'" />
-          <Card :title="'Qualificados'" :icon="'fas fa-user'" :value="qualificado.length" :label="'Dentro do periodo'" />
-          <Card :title="'Em Análise de Crédito'" :icon="'fas fa-user'" :value="analise.length" :label="'Dentro do periodo'" />
+        <Card @click="openModal(leads)" v-if="leads.length > 0" :title="'Todos os leads'" :icon="'fas fa-people-group'"
+          :class="'!bg-gray-500/15 !border-gray-500/30'" :value="leads.length"
+          :label="`De ${dataInicio.split('-').reverse().join('/')} a ${dataFim.split('-').reverse().join('/')}`">
+        </Card>
+        <Card @click="openModal(aguardando)" v-if="aguardando.length > 0" :title="'Aguardando Atendimento'"
+          :icon="'fas fa-stopwatch'" :class="'!bg-gray-500/15 !border-gray-500/30'" :value="aguardando.length"
+          :label="'Aguardando Atendimento Corretor'" />
+        <Card @click="openModal(atendimentos)" v-if="atendimentos.length > 0" :title="'Em Atendimento'"
+          :icon="'fas fa-people-arrows'" :class="'!bg-gray-500/15 !border-gray-500/30'" :value="atendimentos.length"
+          :label="'Em Atendimento com Corretor'" />
+        <Card @click="openModal(descartados)" v-if="descartados.length > 0" :title="'Descartados'"
+          :icon="'fas fa-trash-can'" :class="'!bg-red-500/15 !border-red-500/30'" :value="descartados.length"
+          :label="'Descartados pelo Atendente'" />
+        <Card @click="openModal(qualificado)" v-if="qualificado.length > 0" :title="'Qualificados'"
+          :icon="'fas fa-ranking-star'" :class="'!bg-blue-500/15 !border-blue-500/30'" :value="qualificado.length"
+          :label="'Qualificados pelo Atendente'" />
+      </div>
+      <div class="cards flex w-full gap-4 mb-3">
+        <Card @click="openModal(analise)" v-if="analise.length > 0" :title="'Em Análise de Crédito'"
+          :icon="'fas fa-credit-card'" :class="'!bg-purple-500/15 !border-purple-500/30'" :value="analise.length"
+          :label="'Pasta em Análise Bancária'" />
+        <Card @click="openModal(reserva)" v-if="reserva.length > 0" :title="'Com Reserva'" :icon="'fas fa-tent'"
+          :class="'!bg-yellow-500/20 !border-yellow-500/40'" :value="reserva.length" :label="'Reservas de Unidade'" />
+        <Card @click="openModal(venda)" v-if="venda.length > 0" :title="'Venda Realizada'" :icon="'fas fa-coins'"
+          :class="'!bg-green-500/20 !border-green-500/40'" :value="venda.length" :label="'Vendas Concretizadas'" />
       </div>
 
 
@@ -40,20 +58,12 @@
         <Carregamento v-if="carregando" />
         <div v-if="error" class="text-red-500 my-4">{{ error }}</div>
 
-        <!-- Exibição do período retornado -->
-        <div v-if="periodo.data_inicio && periodo.data_fim" class="mt-6 p-4 dark:bg-gray-800 rounded">
-          <p class="font-semibold">Período:</p>
-          <!-- <p class="text-sm">Início: {{ formatarData(periodo.data_inicio) }}</p>
-          <p class="text-sm">Fim: {{ formatarData(periodo.data_fim) }}</p> -->
-          <p class="text-sm mt-2">Total de Leads: {{ total }}</p>
-        </div>
-
         <!-- Ver relatório detalhado -->
-        <p @click="toggleModal" class="text-blue-500 underline cursor-pointer hover:text-blue-400 text-end pt-2">
+        <p @click="toggleModal(leads)" class="text-blue-500 underline cursor-pointer hover:text-blue-400 text-end pt-2">
           Ver relatório detalhado
         </p>
 
-        <ModalLeads :leads="leads" :modalVisivel="modalVisivel"
+        <ModalLeads :leads="modalLeadsData" :modalVisivel="modalVisivel"
           @update:modalVisivel="(visivel) => (modalVisivel = visivel)" />
 
         <Filas :filas="filas" />
@@ -83,32 +93,48 @@ import ModalLead from "@/components/Leads/ModalLead.vue"; // Importando o novo c
 import Card from "@/components/Leads/Card.vue";
 
 const modalVisivel = ref(false);
+const modalLeadsData = ref([]); // Armazena os leads a serem mostrados no modal
 const store = useLeadsStore();
 const { fetchLeads, fetchFilas } = store;
 const { leads, carregando, error, periodo, total, filas } = storeToRefs(store);
 
-// Supondo que 'leads' já esteja definido como um ref ou importado de algum lugar
 const aguardando = computed(() =>
   leads.value.filter(lead => lead.situacao.nome === 'Aguardando Atendimento Corretor')
 )
-// Supondo que 'leads' já esteja definido como um ref ou importado de algum lugar
 const atendimentos = computed(() =>
   leads.value.filter(lead => lead.situacao.nome === 'Em Atendimento')
 )
-// Supondo que 'leads' já esteja definido como um ref ou importado de algum lugar
 const qualificado = computed(() =>
   leads.value.filter(lead =>
     lead.situacao.nome === 'Lead Qualificado' || lead.situacao.nome === 'Em Negociação'
   )
 )
-// Supondo que 'leads' já esteja definido como um ref ou importado de algum lugar
+const descartados = computed(() =>
+  leads.value.filter(lead => lead.situacao.nome === 'Descartado')
+)
 const analise = computed(() =>
-leads.value.filter(lead => lead.situacao.nome === 'Em Análise de Crédito')
+  leads.value.filter(lead => lead.situacao.nome === 'Em Análise de Crédito')
+)
+const reserva = computed(() =>
+  leads.value.filter(lead => lead.situacao.nome === 'Com Reserva')
+)
+const venda = computed(() =>
+  leads.value.filter(lead => lead.situacao.nome === 'Venda Realizada')
 )
 
 const dataInicio = ref("");
 const dataFim = ref("");
 const mostrarTodos = ref(false);
+
+// Função para abrir o modal com os leads recebidos
+const openModal = (leadsData) => {
+  modalLeadsData.value = leadsData;
+  modalVisivel.value = true;
+};
+// Se desejar também usar a mesma função para o link "Ver relatório detalhado":
+const toggleModal = (leadsData) => {
+  openModal(leadsData);
+};
 
 const buscarLeads = () => {
   fetchLeads({
@@ -127,21 +153,27 @@ const formatarData = (dataString) => {
   return data.toLocaleDateString('pt-BR');
 };
 
+const formatLocalDate = (date) => {
+  const year = date.getUTCFullYear();
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+  const day = date.getUTCDate().toString().padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
+
+
 watch(periodo, (novoPeriodo) => {
   if (novoPeriodo.data_inicio) {
     const dataInicioObj = new Date(novoPeriodo.data_inicio);
-    dataInicio.value = dataInicioObj.toISOString().split('T')[0];
+    dataInicio.value = formatLocalDate(dataInicioObj);
   }
-
   if (novoPeriodo.data_fim) {
     const dataFimObj = new Date(novoPeriodo.data_fim);
-    dataFim.value = dataFimObj.toISOString().split('T')[0];
+    // Subtrai um dia para ajustar o valor final
+    dataFimObj.setUTCDate(dataFimObj.getUTCDate() - 1);
+    dataFim.value = formatLocalDate(dataFimObj);
   }
 }, { immediate: true });
 
-const toggleModal = () => {
-  modalVisivel.value = !modalVisivel.value;
-};
 
 onMounted(() => {
   fetchFilas();
