3b436a2b000771c8cea0d1559e0dd47da0e2d920
GustavoM4tias <faculdogu@gmail.com>
2025-06-23 15:22:07 -0300

    search adjust

---
+++ src/router/searchableRoutes.js
@@ -0,0 +1,50 @@
+export const customSearchableRoutes = [
+    {
+        name: 'Reservas - Imobiliárias',
+        path: '/comercial/reservas',
+        query: { section: 'Imobiliarias' },
+        content: 'Dashboard de reservas por imobiliárias',
+    },
+    {
+        name: 'Reservas',
+        path: '/comercial/reservas',
+        query: { section: 'Reservas' },
+        content: 'Dashboard de reservas',
+    },
+    {
+        name: 'Empreendimentos',
+        path: '/buildings',
+        query: { section: 'Geral' },
+        content: 'Empreendimentos',
+    },
+    {
+        name: 'Empreendimentos',
+        path: '/buildings',
+        query: { section: 'Pré Lançamentos' },
+        content: 'Empreendimentos em pré lançamento',
+    },
+    {
+        name: 'Empreendimentos',
+        path: '/buildings',
+        query: { section: 'Lançamentos' },
+        content: 'Empreendimentos em lançamento',
+    },
+    {
+        name: 'Empreendimentos',
+        path: '/buildings',
+        query: { section: 'Em Obras' },
+        content: 'Empreendimentos em obras',
+    },
+    {
+        name: 'Empreendimentos',
+        path: '/buildings',
+        query: { section: 'Finalizados' },
+        content: 'Empreendimentos finalizados',
+    },
+    {
+        name: 'Empreendimentos',
+        path: '/buildings',
+        query: { section: 'Portal do Cliente' },
+        content: 'Empreendimentos que usam portal do cliente',
+    }
+];
