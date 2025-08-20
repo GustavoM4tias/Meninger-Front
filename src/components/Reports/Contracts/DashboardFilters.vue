4359574430e3110793527093f37281f2cf4f4c1e
Gustavo Henrique Matias Diniz <gustavodiniz200513@gmail.com>
2025-08-20 20:01:55 -0300

    store adapted

--- src/components/Reports/Contracts/DashboardFilters.vue
+++ src/components/Reports/Contracts/DashboardFilters.vue
@@ -25,7 +25,7 @@
                     Situação
                 </label>
                 <select v-model="localFilters.situation"
-                    class="w-full px-2 py-2 border rounded-lg bg-transparent text-gray-400 border-gray-200 dark:border-gray-500 text-center">
+                    class="w-full px-2 py-2 border rounded-lg bg-transparent text-gray-400 border-gray-200 dark:border-gray-500 text-center"> 
                     <option class="text-gray-600" value="">Todas</option>
                     <option class="text-gray-600" value="Emitido">Emitido</option>
                     <option class="text-gray-600" value="Autorizado">Autorizado</option>
