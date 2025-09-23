5dc7cac741ded3a7d655a38bb495114085a517ea
GustavoM4tias <faculdogu@gmail.com>
2025-09-23 12:17:08 -0300

    vercel adapted 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  darkMode: 'class', // Isso permite ativar o modo escuro por uma classe
+  darkMode: 'class', // ativar dark mode via classe
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/flowbite-vue/**/*.{js,vue,ts}"
   ],
   theme: {
     extend: {},
   },
+  plugins: [
+    require("flowbite/plugin")
+  ]
 }
