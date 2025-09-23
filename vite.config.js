b910be50da37cb05bd739bf405a24087418d7dbd
GustavoM4tias <faculdogu@gmail.com>
2025-09-23 12:13:55 -0300

    vercel adapted 3

--- vite.config.js
+++ vite.config.js
@@ -18,39 +18,39 @@ export default defineConfig({
 
     rollupOptions: {
       output: {
-        manualChunks: {
-          // núcleo do framework
-          vue: ['vue'],
+        manualChunks: { 
+        // núcleo do framework
+        vue: ['vue'],
 
-          // gráficos (grande): vai pra um chunk separado e só baixa quando alguma rota que usa eCharts for carregada
-          echarts: ['echarts', 'echarts/core'],
+        // gráficos (grande): vai pra um chunk separado e só baixa quando alguma rota que usa eCharts for carregada
+        echarts: ['echarts', 'echarts/core'],
 
-          // geração de PDF/imagem (grande): isola jspdf e html2canvas
-          pdf: ['jspdf', 'html2canvas'],
+        // geração de PDF/imagem (grande): isola jspdf e html2canvas
+        pdf: ['jspdf', 'html2canvas'],
 
-          // toasts (isola para não ir pro index se for usado pontualmente)
-          toast: ['vue-toastification'],
+        // toasts (isola para não ir pro index se for usado pontualmente)
+        toast: ['vue-toastification'],
 
-          // você pode criar buckets por feature, ex.:
-          // reservas: ['@/pages/Reservas.vue']  // só se fizer sentido estático
-        }
-      },
-      // treeshake já é true por padrão, mas manter explícito não faz mal:
-      // treeshake: 'smallest'
+        // você pode criar buckets por feature, ex.:
+        // reservas: ['@/pages/Reservas.vue']  // só se fizer sentido estáticoqq
+      }
     },
-
-    // dica: se não precisa suportar browsers muito antigos, isso ajuda treeshaking
-    target: 'es2019',
-    minify: 'esbuild'
+    // treeshake já é true por padrão, mas manter explícito não faz mal:
+    // treeshake: 'smallest'
   },
+
+  // dica: se não precisa suportar browsers muito antigos, isso ajuda treeshaking
+  target: 'es2019',
+  minify: 'esbuild'
+},
   server: {
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'https://menin.cvcrm.com.br',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+  host: true,
+  proxy: {
+    '/api': {
+      target: 'https://menin.cvcrm.com.br',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, ''),
     },
   },
+},
 })
