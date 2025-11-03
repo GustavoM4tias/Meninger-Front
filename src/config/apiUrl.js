b286e9be43df634cd3bd18209f6c800ab8beac22
Gustavo Diniz <gustavodiniz200513@gmail.com>
2025-11-03 18:21:37 -0300

    Update apiUrl.js

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,6 +1,6 @@
 // src/config/apiUrl.js
-const API_URL = 'http://localhost:5000/api'; 
+// const API_URL = 'http://localhost:5000/api'; 
 // const API_URL = 'https://node-back-eight.vercel.app/api'; 
-// const API_URL = 'https://menin.up.railway.app/api'
+const API_URL = 'https://menin.up.railway.app/api'
 
 export default API_URL;
